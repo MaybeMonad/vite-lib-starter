671f672992804f60dc09eb93d7d7ad6a22a79f57
Leo <dezineleo@gmail.com>
2021-06-03 10:13:58 +0800

    refactor: update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,43 +1,16 @@
-import React, { useState } from 'react'
-import logo from './logo.svg'
+import React from 'react'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
-
+function App () {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
+    <div className='App'>
+      <h1 className='text-2xl'>Vite-based Starter for Library Development</h1>
+      <p>
+        GitHub:{' '}
+        <a href='https://github.com/jinyang-io/vite-lib-starter'>
+          https://github.com/jinyang-io/vite-lib-starter
+        </a>
+      </p>
     </div>
   )
 }
