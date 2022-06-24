0d0e94df4a8eecf7a0431bc9159e9d7990290d5c
Captain Martin <dezineleo@gmail.com>
2022-06-24 07:14:11 +0000

    chore: upgrade

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,29 @@
 import { defineConfig } from 'vite'
 import reactRefresh from '@vitejs/plugin-react-refresh'
-const path = require('path')
+import path from 'path'
+import packageJson from "./package.json";
+
+const getPackageName = () => {
+  return packageJson.name;
+};
+
+const getPackageNameCamelCase = () => {
+  try {
+    return getPackageName().replace(/-./g, (char) => char[1].toUpperCase());
+  } catch (err) {
+    throw new Error("Name property in package.json is missing.");
+  }
+};
+
+const fileName = {
+  es: `${getPackageName()}.mjs`,
+  cjs: `${getPackageName()}.cjs`,
+  iife: `${getPackageName()}.iife.js`,
+};
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: './',
   plugins: [reactRefresh()],
   resolve: {
     alias: {
@@ -14,7 +34,9 @@ export default defineConfig({
   build: {
     lib: {
       entry: path.resolve(__dirname, 'src/index.ts'),
-      name: 'vite-lib-starter'
+      name: getPackageNameCamelCase(),
+      formats: ["es", "cjs", "iife"],
+      fileName: (format) => fileName[format],
     },
     rollupOptions: {
       external: ['react', 'react-dom'],
